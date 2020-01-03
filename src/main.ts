4916e52cf2e3c725aa5d8f12f6ef8bcb4a3cf739
mallowigi <heliosaian@gmail.com>
2020-01-03 19:55:39 +0200

    ...

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,12 @@
-import { ValidationPipe } from '@nestjs/common';
-import { Transport }      from '@nestjs/common/enums/transport.enum';
-import { NestFactory }    from '@nestjs/core';
-import { join }           from 'path';
-import { AppModule }      from './app.module';
+import { authorizationGrpcClientOptions } from '@micro/common/dist/src';
+import { ValidationPipe }                 from '@nestjs/common';
+import { NestFactory }                    from '@nestjs/core';
+import { Transport }                      from '@nestjs/microservices';
+import { AppModule }                      from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.connectMicroservice({
-    transport: Transport.GRPC,
-    options:   {
-      package:   'service',
-      url:       '0.0.0.0:50050',
-      protoPath: join(__dirname, '../../common/proto/authorization/service.proto'),
-    },
-  });
+  app.connectMicroservice(authorizationGrpcClientOptions);
   app.connectMicroservice({
     transport: Transport.NATS,
     options:   {
@@ -29,7 +22,7 @@ async function bootstrap() {
     }),
   );
   // tslint:disable-next-line:no-console
-  app.startAllMicroservices(() => console.log('Running authorization microservice'));
+  await app.startAllMicroservicesAsync();
   await app.listen(3000);
 }
 
